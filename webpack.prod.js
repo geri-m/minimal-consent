be213b78134d4b698e31c421b3d29c0bc71b1eba
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2020-01-26 09:40:45 +0100

    Remove Logging in Production

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const merge = require('webpack-merge');
 const parentWebPack = require('./webpack.common.js');
 const JavaScriptObfuscator = require('webpack-obfuscator');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(parentWebPack, {
     mode: 'production',
@@ -11,5 +12,17 @@ module.exports = merge(parentWebPack, {
             {from: './src/manifest.prod.json', to: './manifest.json'},
             {from: './src/images/', to: './images/'}]),
         new JavaScriptObfuscator({rotateUnicodeArray: true})
-    ]
+    ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true, // Must be set to true if using source-maps in production
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                },
+            }),
+        ],
+    },
 });
