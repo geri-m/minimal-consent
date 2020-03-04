0bff52b7a6e8b1bfcf91c6cd5e236ac8c9bddd6d
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2020-03-04 21:13:28 +0100

    Fix Evidon, Fis Spanish Translations

--- src/js/cmp/Evidon.js
+++ src/js/cmp/Evidon.js
@@ -10,22 +10,35 @@ export default class Evidon extends CMP {
         super(18, node, "Evidon, Inc.", scriptUrl, CMP.cmpType.DO_NOT_WAIT, true, backendCall);
     }
 
-    handleCmp() {
+    async handleCmp() {
         const evidonDenyAll = "button#_evidon-decline-button";
-
         let button = super.queryNodeSelector(evidonDenyAll);
 
+        const evidonOptions = "button#_evidon-option-button";
+        let evidonOptionsButton = super.queryNodeSelector(evidonOptions);
+
+        const evidonDeclineAll = "button#evidon-prefdiag-decline";
+        let evidonDenyAllButton = super.queryNodeSelector(evidonDeclineAll);
+
         // we do require 3 attempts to decline the tracking
-        if (Utils.objectClickable(button) && super.state === 0) {
+        if (Utils.objectClickable(evidonOptionsButton) && super.state === 0) {
+            Utils.log("Clicking Button 1");
             super.state = 1;
-            button.click();
-        } else if (Utils.objectClickable(button) && super.state === 1) {
-            super.state = 2;
-            button.click();
-        } else if (Utils.objectClickable(button) && super.state === 2) {
-            super.state = 3;
+            evidonOptionsButton.click();
+        } else if (Utils.objectClickable(evidonDenyAllButton) && super.state === 1) {
+            await this.sleep(1000); // Pausiert die Funktion für 3 Sekunden
+            Utils.log("Clicking Button 2");
+            evidonDenyAllButton.click();
+            super.reset();
+        } else if (Utils.objectClickable(button) && super.state === 0) {
+            Utils.log("Clicking Button xxx");
             button.click();
             super.reset();
         }
     }
+
+    sleep(milliseconds) {
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+
 }
