abac8be1c56212cb82c5996f27623e18fdf54cd4
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2020-04-05 19:19:39 +0200

    Remove Migration from Test

--- src/test/tests/integration/history.spec.ts
+++ src/test/tests/integration/history.spec.ts
@@ -2,7 +2,6 @@
 
 import History from "../../../js/background/History";
 import HistoryEntry from "../../../js/entities/HistoryEntry";
-import Utils from "../../../js/Utils";
 import PingResult from "../../../js/entities/PingResult";
 
 describe('History', () => {
@@ -49,25 +48,6 @@ describe('History', () => {
     });
 
 
-/*
-    it("Simple History Tests", async function () {
-
-        let result : any;
-        chrome.storage.sync.set({"entry": {"key": "value"}}, function () {
-            console.log('Saved new history object to Chrome Storage.' + chrome.runtime.lastError.message);
-        });
-
-        chrome.storage.sync.get("entry", function (r: { [id: string]: any }) {
-            console.log("load: Data in Storage: " + JSON.stringify(result) + " " + chrome.runtime.lastError.message);
-            result = r;
-        });
-
-        console.log("Result: " + result);
-
-        expect(result).toBe(JSON.parse('{"key": "value"}'));
-    });
-*/
-
     it('Multiple History Entries in one Storage', async function () {
         let hist = new History();
         let pr = new PingResult(false, true, false, "cmpStatus", "displayStatus", "apiVersion", 1, 2, 3, 4);
@@ -98,91 +78,4 @@ describe('History', () => {
         expect(blockCount).toBe(2);
     });
 
-    it('Test Migration of History', async function () {
-        let hist = new History();
-
-        // create an empty array for the History Key.
-        let result: { [id: string]: any[]; } = {
-            history: new Array<any>()
-        };
-
-        let cmp1: { [id: string]: any; } = {
-            cmpId: 412
-        };
-
-        let entry1: { [id: string]: any; } = {
-            cmp: "Some CMP",
-            cmpScriptUrl: "request.cmpScripUrl",
-            date: "2020-03-01 17:37:45",
-            implemented: false,
-            pingResult: cmp1,
-            url: "www.orf.at"
-        };
-
-        let cmp2: { [id: string]: any; } = {
-            cmpId: 41
-        };
-
-        let entry2: { [id: string]: any; } = {
-            cmp: "TrustArc Inc",
-            cmpScriptUrl: "//consent.truste.com/notice?domain=forbes.com&c=teconsent",
-            date: "2020-03-01 17:37:57",
-            implemented: true,
-            pingResult: cmp2,
-            url: "www.forbes.com"
-        };
-
-        let cmp3: { [id: string]: any; } = {
-            cmpId: 5,
-            cmpLoaded: false,
-            gdprAppliesGlobally: false
-        };
-
-        let entry3: { [id: string]: any; } = {
-            cmp: "Usercentrics GmbH",
-            cmpScriptUrl: "https://app.usercentrics.eu/latest/main.js",
-            date: "2020-03-01 17:37:45",
-            implemented: true,
-            pingResult: cmp3,
-            url: "www.usercentrics.com"
-        };
-
-        result.history.push(entry1);
-        result.history.push(entry2);
-        result.history.push(entry3);
-
-        // we use a separated store function, as 'save()' in the history object, used load() which creates an
-        // array of HistoryEntries.
-        await storeObject(result);
-        await hist.doMigration();
-
-        let forbes = await hist.getLastFound("www.forbes.com");
-        expect(forbes).not.toBeNull();
-        console.log("Forbes: " + forbes);
-        let forbesObject = HistoryEntry.class(forbes);
-        expect(forbesObject.pingResult.cmpId).toBe(41);
-
-        let uc = await hist.getLastFound("www.usercentrics.com");
-        expect(uc).not.toBeNull();
-        console.log("UC: " + uc);
-        let ucObject = HistoryEntry.class(uc);
-        expect(ucObject.pingResult.cmpId).toBe(5);
-
-    });
-
-
 });
-
-
-async function storeObject(store: any) {
-    return new Promise(function (resolve, reject) {
-        chrome.storage.sync.set(store, function () {
-            Utils.log('Saved new history object to Chrome Storage.');
-            // store worked, resolve now.
-            resolve();
-        });
-    });
-}
-
-
-
