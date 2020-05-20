2316f888accad5e66046bcf61ec08c0905633456
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2020-05-20 08:51:58 +0200

    Trim AutoReload webpack

--- webpack.start.js
+++ webpack.start.js
@@ -1,14 +1,9 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackExtensionReloaderPlugin = require('webpack-extension-reloader');
 const merge = require('webpack-merge');
-const parentWebPack = require('./webpack.common.js');
+const parentWebPackDev = require('./webpack.dev.js');
 
-module.exports = merge(parentWebPack, {
-    mode: 'development',
-    // FIX: Module not found: Error: Can't resolve 'fs'
-    node: {fs: 'empty'},
-    // ATTENTION: If we change this, the integration pages are not shown any more
-    devtool: 'inline-source-map',
+module.exports = merge(parentWebPackDev, {
     plugins: [
         // Auto Start
         new WebpackExtensionReloaderPlugin({
@@ -16,25 +11,6 @@ module.exports = merge(parentWebPack, {
                 contentScript: 'contentscript',
                 background: 'background'
             }
-        }),
-
-        new CopyWebpackPlugin([
-            /* This is required for Integration Testing */
-            {
-                from: './node_modules/jasmine-core/lib/jasmine-core/*.*',
-                to: './test/jasmine-core/',
-                flatten: true,
-            },
-            {from: './src/manifest.dev.json', to: './manifest.json'},
-            {from: './src/images/', to: './images/'},
-            {from: './src/js/popup/popup.html', to: './popup/'},
-            {from: './src/js/options/options.html', to: './options/'},
-            {from: './src/test/', to: './test/'},
-            {from: './src/_locales/', to: './_locales/'}])
-    ],
-    /* This is how the test Page will be available on the page */
-    entry: {
-        './test/tests/integration/history.spec': './src/test/tests/integration/history.spec.ts',
-        './test/tests/integration/deviceId.spec': './src/test/tests/integration/deviceId.spec.ts',
-    }
+        })
+    ]
 });
